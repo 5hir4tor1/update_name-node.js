9ed5559b55fe648e438c8fa65a74ecc2563ab7de
thr3a <example@example.com>
2014-09-29 10:52:16 +0900

    正規表現を多スペースに対応　update_name reset追加　Switch構文化

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var Twit = require('twit');
-//自身のスクリーンネーム
+var Twit = require('twit')
 var screen_name = '';
+var defaultName = '';
 
 var tw = new Twit({
 	consumer_key: '',
@@ -10,18 +10,27 @@ var tw = new Twit({
 });
 
 var stream = tw.stream('user');
+console.log('starting update_name for @' + screen_name);
 stream.on('tweet', function(data) {
-	//「@~~~ update_name ****」のツイートに反応
-	var reg = new RegExp("^@" + screen_name + "[ 　]update_name[ 　]+(.+)");
-	var newname = data.text.match(reg);
-	if(newname !== null){
-		//厄介対策　命名から@を削除
-		newname = newname[1].replace(/[@＠]/g, "");
-		newname = newname.slice(0,20);
-		tw.post('account/update_profile', { name: newname	}, function(err, rep) {
-			var message = '.@' + data.user.screen_name + ' さんにより 『' + newname + '』になりました!';
-			tw.post('statuses/update', { status: message, in_reply_to_status_id: data.id_str }, function(err, rep) {
-			});
-		});
-	};
+	var reg = new RegExp("^@" + screen_name + "[ 　]+(.+[^ 　])[ 　]+(.+)");
+	var match = data.text.match(reg);
+	if(match){
+		var command = match[1];
+		switch(command){
+			//名前変更
+			case 'update_name':
+				//厄介対策　命名から@を削除
+				newname = match[2].replace(/[@＠]/g, "");
+				newname = newname.slice(0,20);
+				if(newname === 'reset'){
+					newname = defaultName;
+				}
+				tw.post('account/update_profile', { name: newname	}, function(err, rep) {
+					var message = '.@' + data.user.screen_name + ' さんにより 『' + newname + '』になりました!';
+					tw.post('statuses/update', { status: message, in_reply_to_status_id: data.id_str }, function(err, rep) {
+					});
+				});
+			break;
+		}
+	}
 });
